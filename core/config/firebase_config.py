816a331f5d3177e1f29590289f8fa514cfadfb40
Caroline Shingai <carolineshingai@gmail.com>
2025-05-01 16:03:10 -0300

    database implemented, working and already connect with the chatbot

--- core/config/firebase_config.py
+++ core/config/firebase_config.py
@@ -1,13 +1,31 @@
 import firebase_admin
 from firebase_admin import credentials, firestore
+from dotenv import load_dotenv
 import os
 from pathlib import Path
+import json
 
-current_dir = Path(__file__).parent
-cred_path = current_dir.parent.parent / "credentials" / "furia-firebase-admin.json"
+load_dotenv()
 
-print(cred_path)
-cred = credentials.Certificate(str(cred_path))
-firebase_admin.initialize_app(cred)
+def initialize_firebase():
+    firebase_config = {
+        "type": os.getenv("FIREBASE_TYPE"),
+        "project_id": os.getenv("FIREBASE_PROJECT_ID"),
+        "private_key_id": os.getenv("FIREBASE_PRIVATE_KEY_ID"),
+        "private_key": os.getenv("FIREBASE_PRIVATE_KEY"),
+        "client_email": os.getenv("FIREBASE_CLIENT_EMAIL"),
+        "client_id": os.getenv("FIREBASE_CLIENT_ID"),
+        "auth_uri": os.getenv("FIREBASE_AUTH_URI"),
+        "token_uri": os.getenv("FIREBASE_TOKEN_URI"),
+        "auth_provider_x509_cert_url": os.getenv("FIREBASE_AUTH_PROVIDER_X509_CERT_URL"),
+        "client_x509_cert_url": os.getenv("FIREBASE_CLIENT_X509_CERT_URL")
+    }
+    if None in firebase_config.values():
+        missing = [k for k, v in firebase_config.items() if v is None]
+        raise ValueError(f"Variáveis do Firebase faltando no .env: {missing}")
+    
+    cred = credentials.Certificate(firebase_config)
+    return firebase_admin.initialize_app(cred)
 
+app = initialize_firebase()
 db = firestore.client()
