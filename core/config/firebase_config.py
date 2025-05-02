ac7654446d1f7d4263ae24cca1e4e4d96a49bf5c
CarolShingai <carolineshingai@gmail.com>
2025-05-01 22:37:12 -0300

    inicio do front

--- core/config/firebase_config.py
+++ core/config/firebase_config.py
@@ -5,27 +5,27 @@ import os
 from pathlib import Path
 import json
 
-load_dotenv()
+# load_dotenv()
 
-def initialize_firebase():
-    firebase_config = {
-        "type": os.getenv("FIREBASE_TYPE"),
-        "project_id": os.getenv("FIREBASE_PROJECT_ID"),
-        "private_key_id": os.getenv("FIREBASE_PRIVATE_KEY_ID"),
-        "private_key": os.getenv("FIREBASE_PRIVATE_KEY"),
-        "client_email": os.getenv("FIREBASE_CLIENT_EMAIL"),
-        "client_id": os.getenv("FIREBASE_CLIENT_ID"),
-        "auth_uri": os.getenv("FIREBASE_AUTH_URI"),
-        "token_uri": os.getenv("FIREBASE_TOKEN_URI"),
-        "auth_provider_x509_cert_url": os.getenv("FIREBASE_AUTH_PROVIDER_X509_CERT_URL"),
-        "client_x509_cert_url": os.getenv("FIREBASE_CLIENT_X509_CERT_URL")
-    }
-    if None in firebase_config.values():
-        missing = [k for k, v in firebase_config.items() if v is None]
-        raise ValueError(f"Variáveis do Firebase faltando no .env: {missing}")
-    
-    cred = credentials.Certificate(firebase_config)
-    return firebase_admin.initialize_app(cred)
+def get_firestore_db():
+#     if not firebase_admin._apps:
+#         firebase_config = {
+#             "type": os.getenv("FIREBASE_TYPE"),
+#             "project_id": os.getenv("FIREBASE_PROJECT_ID"),
+#             "private_key_id": os.getenv("FIREBASE_PRIVATE_KEY_ID"),
+#             "private_key": os.getenv("FIREBASE_PRIVATE_KEY", "").replace('\\n', '\n'),
+#             "client_email": os.getenv("FIREBASE_CLIENT_EMAIL"),
+#             "client_id": os.getenv("FIREBASE_CLIENT_ID"),
+#             "auth_uri": os.getenv("FIREBASE_AUTH_URI"),
+#             "token_uri": os.getenv("FIREBASE_TOKEN_URI"),
+#             "auth_provider_x509_cert_url": os.getenv("FIREBASE_AUTH_PROVIDER_X509_CERT_URL"),
+#             "client_x509_cert_url": os.getenv("FIREBASE_CLIENT_X509_CERT_URL")
+#         }
+#         cred = credentials.Certificate(firebase_config)
+#         firebase_admin.initialize_app(cred)
 
-app = initialize_firebase()
-db = firestore.client()
+    cred = credentials.Certificate("credentials/furia-chatbot-firebase-admin.json")
+    firebase_admin.initialize_app(cred)
+    return firestore.client()
+
+db = get_firestore_db()
